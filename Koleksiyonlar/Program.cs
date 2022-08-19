e52195ff3fe9f07c9e8d68875df3a7bf6796d7f0
iremn <iremn@DESKTOP-RRSQ6JC>
2022-08-19 16:35:58 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+//string[] isimler  = new string[]{"engin","murat","kerem"};
+//console.writeline(isimler[0]);
+//console.writeline(isimler[1]);
+//console.writeline(isimler[2]);
+
+isimler[3] = "ilker"; // yapamayız çünkü array de yok sınır dışına çıkamaz
+
+List<string> isimler2 = new List<string>();
+
+    
